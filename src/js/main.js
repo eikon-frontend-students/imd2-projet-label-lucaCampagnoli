3579fd2f681f8c3e082685b6a006b24c84bd8200
Luca Campagnoli <luca.campagnoli@studentfr.ch>
2025-12-10 11:22:22 +0100

    homepage-fini

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,19 @@
-document.querySelectorAll(".title-img").forEach((img) => {
-  img.addEventListener("mousemove", (e) => {
-    const rect = img.getBoundingClientRect();
+document.querySelectorAll(".trav").forEach((link) => {
+  const img = link.querySelector("img"); // l'image à l'intérieur
+
+  link.addEventListener("mousemove", (e) => {
+    const rect = link.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    const offsetX = (x - rect.width / -2) / 80;
-    const offsetY = (y - rect.height / -2) / 5;
+    const offsetX = (x - rect.width / -3) / 80;
+    const offsetY = (y - rect.height / -3) / 8;
 
-    const distanceToCenter = Math.sqrt(offsetX ** 2 + offsetY ** 2);
-    const scale = 1 - Math.min(distanceToCenter / 10, 0.05);
-
-    img.style.transform = `rotateY(${offsetX}deg) rotateX(${-offsetY}deg) scale(${scale})`;
-    img.style.filter = "brightness(1.2)"; // éclaircissement pendant hover
+    img.style.transform = `rotateY(${offsetX}deg) rotateX(${-offsetY}deg) scale(1.05)`;
+    img.style.filter = "brightness(1.2)";
   });
 
-  img.addEventListener("mouseleave", () => {
+  link.addEventListener("mouseleave", () => {
     img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
     img.style.filter = "brightness(1)";
   });
