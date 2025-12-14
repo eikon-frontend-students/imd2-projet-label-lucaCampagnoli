14c70f699503904960dc09bcfded8571916f316e
Luca Campagnoli <luca.campagnoli@studentfr.ch>
2025-12-14 14:58:43 +0100

    homepage_terminé

--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,11 @@ document.querySelectorAll(".trav").forEach((link) => {
     const offsetY = (y - rect.height / -3) / 8;
 
     img.style.transform = `rotateY(${offsetX}deg) rotateX(${-offsetY}deg) scale(1.05)`;
-    img.style.filter = "brightness(1.2)";
+    img.style.filter = "brightness(1.5)";
   });
 
   link.addEventListener("mouseleave", () => {
-    img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
-    img.style.filter = "brightness(1)";
+    img.style.transform = "rotateY(-20deg) rotateX(30deg) scale(1)";
+    img.style.filter = "brightness(1.2)";
   });
 });
