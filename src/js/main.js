8d8cac06d03c4ff8c0f998940c6616db8f9ff12b
Luca Campagnoli <luca.campagnoli@studentfr.ch>
2025-12-14 23:45:53 +0100

    js-fait

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,24 @@ document.querySelectorAll(".trav").forEach((link) => {
     img.style.filter = "brightness(1.2)";
   });
 });
+
+const imgs = document.querySelectorAll(".albums img, .diff-wrap img");
+
+imgs.forEach((img) => {
+  img.addEventListener("mousemove", (e) => {
+    const rect = img.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * 10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    img.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+  });
+
+  img.addEventListener("mouseleave", () => {
+    img.style.transform = "rotateX(0deg) rotateY(0deg) scale(1)";
+  });
+});
